c202bc7ff1d7884726c6a8f84e8e62a4039e9296
Barsha Magar <barshamagar@Barshas-MacBook-Air.local>
2025-05-26 20:55:29 +0545

    makes changs

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-print("hello world")
-a="string"
-print(type(a))
-
-
-
+# print("hello world")
+# a="string"
+# print(type(a))
+# cup="coffee"
+# print(cup)
+# a=1
+# print(type(a))
+# for i in range(5):
+#     print("hi")
+#     print("hello")
+# isNepali=True
+# print(isNepali)
+# Barsha="barsha"
+# print("my Barsha is:"+Barsha)
+# print("my Barsha is:", Barsha)
+# x,y,z="orange","banana","cherry"
+# print(x,y,z)
+# x=y=z="barsha"
+# print(x,y,z)
+# fruits="apple","banana","Cherry"
+# a,b,c=fruits
+# print(a,b,c)
+# x=2
+# y=10
+# print(x+y)
+friends=["barsha","kabita","ramita","gita",1,True]
+print(friends)
+print(friends[1])
+print(friends[5])
+print(len(friends))
+print(friends[-1])
