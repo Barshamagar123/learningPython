fb84e437d375156f7bed1374683b4cb4f7817549
Barsha Magar <barshamagar@Barshas-MacBook-Air.local>
2025-05-27 21:03:43 +0545

    makes changes

--- day-1.py
+++ day-1.py
@@ -5,12 +5,54 @@
 # print(a[3:6])
 # print(a[2:])
 # print(type(a))
-
+# barsha={
+#     'name':'sita',
+#     'age':22
+# }
+# print(barsha)
+# print(barsha['name'])
+# if 'age' in barsha:
+#     print("yes is it in barsha")
+# barsha['faculty']='bit'
+# print(barsha)
 # input("enter a number")
 # print("thank you for typing the number")
 # input("enter a ")
-a='abc'
-print(a.upper())
-print(a.lower())
-if 'a'.islower():
-    print('a is lower')
+# a='abc'
+# print(a.upper())
+# print(a.lower())
+# if 'a'.islower():
+#     print('a is lower')
+
+
+# user input
+# name=input("enter your name")
+# print(f"my nam eis {name} ")
+
+
+# name=input("enter your name")
+# print(f"my name is {name}")
+# print("my name is:" +name)
+
+# num1=int((input("enter one number")))
+# num2=int((input("enter second number")))
+# num3=num1+num2
+#
+# print(num3)
+
+# a=10
+# if a%2==0:
+#     print("Even numbers")
+
+# check is it is a even or odd number
+# num=int(input("enter a number"))
+# if num%2==0:
+#     print("even number")
+# else:
+#     print("odd number")
+
+age=int(input("enter your age"))
+if age>=18:
+    print("you are elligible for vote ")
+else:
+    print("you are not elligible for vote ")
