07cd4acd477d8c395e955533367f9467d400d5c6
Barsha Magar <barshamagar@Barshas-MacBook-Air.local>
2025-05-27 10:47:58 +0545

    makes changes

---
+++ day-1.py
@@ -0,0 +1,16 @@
+# a,b,c=['barsha','sita','gita']
+# print(a,b,c)
+# a="barsha"
+# print(a[0:3])
+# print(a[3:6])
+# print(a[2:])
+# print(type(a))
+
+# input("enter a number")
+# print("thank you for typing the number")
+# input("enter a ")
+a='abc'
+print(a.upper())
+print(a.lower())
+if 'a'.islower():
+    print('a is lower')
