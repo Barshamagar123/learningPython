10b313601d3c1297c74fb087c5d07a7ff7349946
Barsha Magar <magarbarsha3333@gmail.com>
2026-08-23 17:45:33 +0545

    add mathmodule and shorthandoperator files with basic math operations

---
+++ week1/shorthandoperator.py
@@ -0,0 +1,20 @@
+x=15
+
+#additional
+# x+=5
+# print(x)
+
+#subtraction assignment
+# x-=6
+# print(x)
+
+#multiplication assignment
+# x/=5
+# print(x)
+
+#floor divison assingment
+x//=4
+print(x)
+
+
+
