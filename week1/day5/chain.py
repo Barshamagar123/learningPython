1d4a1382f4ca62d2a21c0ca0f2cf98e4f05ef497
Barsha Magar <magarbarsha3333@gmail.com>
2026-09-02 08:05:43 +0545

    learning more about none, identity vs equality , chain comparison

---
+++ week1/day5/chain.py
@@ -0,0 +1,7 @@
+x=5
+if x>0 and x<10:
+    print("x is between 0 and 10")
+if 0<x<10:
+    print("x is between 0 and 10")
+if 0<x<10<20:
+    print("all true")
---
+++ week1/day5/comparisonoperators.py
@@ -0,0 +1,12 @@
+a=5
+b=8
+c=9
+d="7"
+print(a==b)
+print(a!=b)
+
+print(a>b)
+print(a<b)
+print(a>=b)
+print(c==d)
+print(type(d))
