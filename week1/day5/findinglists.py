3b0a188ca9595c6edc4c4f323ad2c6c5f560b19c
Barsha Magar <magarbarsha3333@gmail.com>
2026-09-02 08:42:55 +0545

    learning fining lists elements and membership testing

---
+++ week1/day5/findinglists.py
@@ -0,0 +1,5 @@
+fruits=['apple','bannanan','mango','lemon','apple']
+print(fruits.index('apple'))
+print(fruits.index('lemon'))
+print(fruits.index('apple',1))
+print(fruits.count('apple'))
---
+++ week1/day5/memebership.py
@@ -0,0 +1,9 @@
+fruits=['bananna','apple','orange']
+print('apple' in fruits)
+print('graphes' in fruits)
+print('apple' not in fruits)
+
+#nested lists
+matrix=[[1,2],[3,4]]
+print([1,2] in matrix)
+print([5,6] in matrix)
